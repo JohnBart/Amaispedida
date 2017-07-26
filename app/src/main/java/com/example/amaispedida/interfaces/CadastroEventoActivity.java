cfb578311078f8f3c517c4e1179776d4af5684b4
João Paulo Bart Dias <joaopaulobart@hotmail.com>
2017-07-26 13:40:07 -0300

    campo cadastro tem que ser totalmente preenchido

--- app/src/main/java/com/example/amaispedida/interfaces/CadastroEventoActivity.java
+++ app/src/main/java/com/example/amaispedida/interfaces/CadastroEventoActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.amaispedida.R;
 import com.example.amaispedida.database.DatabaseHelper;
@@ -22,6 +24,7 @@ public class CadastroEventoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+
             }
         });
 
@@ -30,6 +33,20 @@ public class CadastroEventoActivity extends AppCompatActivity {
         bt_salvarEvento.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText nome_evento = (EditText) findViewById(R.id.et_nome_evento_cadastro);
+                EditText local = (EditText) findViewById(R.id.et_local_evento_cadastro);
+                EditText data = (EditText) findViewById(R.id.et_data_evento_cadastro);
+                EditText hora = (EditText) findViewById(R.id.et_hora_evento_cadastro);
+
+                String nome_eventost = nome_evento.getText().toString();
+                String localst = local.getText().toString();
+                String datast = data.getText().toString();
+                String horast = hora.getText().toString();
+
+                if(nome_eventost.isEmpty()||localst.isEmpty()||datast.isEmpty()||horast.isEmpty()) {
+                    alert("Campo em branco, favor preencher todo o formulário");
+                }
+
 
 
             }
@@ -51,5 +68,8 @@ public class CadastroEventoActivity extends AppCompatActivity {
 
             }
         });
+    }
+    public void alert(String str){
+        Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
     }
 }
